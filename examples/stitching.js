b94422d026ffcef6f362802f0af7adb3d608e323
Marcel Erz <me@marcelerz.com>
2015-04-19 03:36:22 -0700

    Add some more browsers

--- examples/stitching.js
+++ examples/stitching.js
@@ -13,7 +13,10 @@ var chromedriver = require('chromedriver');
 //var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'internet explorer', version:'9.0', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
 //var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'safari', version:'5.1', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
 //var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'opera', version:'12.12', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'safari', version:'8.0', platform: 'OS X 10.10' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
+//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'safari', version:'8.0', platform: 'OS X 10.10' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
+//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPad Simulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
+//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'android', deviceName:'Android Emulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
+//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'android', platform:'Linux', version:'4.4', deviceName:'Samsung Galaxy S4 Emulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
 
 var browser = driver.browser();
 var activeWindow = browser.activeWindow();
@@ -23,12 +26,14 @@ activeWindow.navigator().setUrl('http://www.yahoo.com');
 
 activeWindow.saveScreenshot(__dirname + '/' + driver.browserName() + " " + driver.browserVersion() + " " + driver.platform() + '.png', {
     eachFn: function () {
-        if (arguments[0] >= 1) {
+        if ((arguments[0] >= 1) && (document.getElementById('masthead'))) {
             document.getElementById('masthead').style.display = 'none';
         }
     },
     completeFn: function () {
-        document.getElementById('masthead').style.display = '';
+        if (document.getElementById('masthead')) {
+            document.getElementById('masthead').style.display = '';
+        }
     }
 });
 
