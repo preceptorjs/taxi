d75289947931a58d09c839368baa0ad4e944db05
Marcel Erz <me@marcelerz.com>
2015-04-20 23:00:43 -0700

    Fixing iPhone screenshots + some other cleanup

--- examples/stitching.js
+++ examples/stitching.js
@@ -1,42 +1,72 @@
 var taxi = require('..');
 var chromedriver = require('chromedriver');
 
+var user = process.env.SAUCE_USERNAME;
+var accessKey = process.env.SAUCE_ACCESS_KEY;
 
-//chromedriver.start();
+var sauceLabsUrl = "http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub";
 
-//var driver = taxi('http://localhost:9515/', { browserName:'chrome' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi('http://localhost:4444/wd/hub', { browserName:'firefox' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'chrome', version:'41.0', platform: 'Windows 8.1' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'firefox', version:'37.0', platform: 'Windows 8.1' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'internet explorer', version:'11.0', platform: 'Windows 8.1' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'internet explorer', version:'10.0', platform: 'Windows 8' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'internet explorer', version:'9.0', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'safari', version:'5.1', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'opera', version:'12.12', platform: 'Windows 7' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'safari', version:'8.0', platform: 'OS X 10.10' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPad Simulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'android', deviceName:'Android Emulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
-//var driver = taxi("http://" + user + ":" + accessKey + "@ondemand.saucelabs.com/wd/hub", { browserName:'android', platform:'Linux', version:'4.4', deviceName:'Samsung Galaxy S4 Emulator' }, { mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true });
+var tests = [
+    //{ url:'http://localhost:9515/', capabilities: { browserName:'chrome' }, beforeFn: function () { chromedriver.start(); }, afterFn: function () { chromedriver.stop() } },
+    //
+    //{ url:'http://localhost:9517/', capabilities: { browserName:'phantomjs', browserVersion:'1.9.8' } },
+    //{ url:'http://localhost:4444/wd/hub', capabilities: { browserName:'firefox' } },
+    //
+    //{ url:'http://makingshaking.corp.ne1.yahoo.com:4444', capabilities: { browserName:'phantomjs', browserVersion: '2.0.0 dev' } },
+    //
+    //{ url:sauceLabsUrl, capabilities: { browserName:'chrome', version:'41.0', platform:'Windows 8.1' } },
+    //
+    //{ url:sauceLabsUrl, capabilities: { browserName:'firefox', version:'37.0', platform:'Windows 8.1' } },
+    //
+    //{ url:sauceLabsUrl, capabilities: { browserName:'internet explorer', version:'11.0', platform:'Windows 8.1' } },
+    //{ url:sauceLabsUrl, capabilities: { browserName:'internet explorer', version:'10.0', platform:'Windows 8' } },
+    //{ url:sauceLabsUrl, capabilities: { browserName:'internet explorer', version:'9.0', platform:'Windows 7' } },
+    //
+    //{ url:sauceLabsUrl, capabilities: { browserName:'safari', version:'5.1', platform:'Windows 7' } },
+    //{ url:sauceLabsUrl, capabilities: { browserName:'safari', version:'8.0', platform:'OS X 10.10' } },
+    //
+    //{ url:sauceLabsUrl, capabilities: { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPad Simulator', "device-orientation":'portrait' } },
+    //{ url:sauceLabsUrl, capabilities: { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPad Simulator', "device-orientation":'landscape' } },
+    { url:sauceLabsUrl, capabilities: { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPhone Simulator', "device-orientation":'portrait' } },
+    //{ url:sauceLabsUrl, capabilities: { browserName:'iphone', version:'8.2', platform: 'OS X 10.10', deviceName:'iPhone Simulator', "device-orientation":'landscape' } }
+];
 
-var browser = driver.browser();
-var activeWindow = browser.activeWindow();
+tests.forEach(function (test) {
 
-// Set url and assert a header-text
-activeWindow.navigator().setUrl('http://www.yahoo.com');
+    if (test.beforeFn) {
+        test.beforeFn();
+    }
 
-activeWindow.saveScreenshot(__dirname + '/' + driver.browserName() + " " + driver.browserVersion() + " " + driver.platform() + '.png', {
-    eachFn: function () {
-        if ((arguments[0] >= 1) && (document.getElementById('masthead'))) {
-            document.getElementById('masthead').style.display = 'none';
-        }
-    },
-    completeFn: function () {
-        if (document.getElementById('masthead')) {
-            document.getElementById('masthead').style.display = '';
+    try {
+        var driver = taxi(test.url, test.capabilities, {mode: taxi.Driver.MODE_SYNC, debug: true, httpDebug: true});
+
+        var browser = driver.browser();
+        var activeWindow = browser.activeWindow();
+
+        // Set url and assert a header-text
+        activeWindow.navigator().setUrl('http://www.yahoo.com');
+
+        var browserId = (driver.deviceName() != '' ? driver.deviceName() : driver.browserName()) + " " + driver.deviceOrientation() + " " + driver.browserVersion() + " " + driver.platform();
+        activeWindow.saveScreenshot(__dirname + '/' + browserId.trim() + '.png', {
+            eachFn: function () {
+                if (arguments[0] >= 1 && document.getElementById('masthead')) {
+                    document.getElementById('masthead').style.display = 'none';
+                }
+            }, completeFn: function () {
+                if (document.getElementById('masthead')) {
+                    document.getElementById('masthead').style.display = '';
+                }
+            }
+        });
+    } catch (err) {
+        console.error(err.stack);
+
+    } finally {
+
+        driver.dispose();
+
+        if (test.afterFn) {
+            test.afterFn();
         }
     }
 });
-
-driver.dispose();
-
-//chromedriver.stop();
